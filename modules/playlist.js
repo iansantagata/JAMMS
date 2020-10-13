4a93f8b88b9c7d1ddb6e5e90318f263c17398852
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2020-10-12 23:30:54 -0400

    Refactored style with bootstrap using grid and overhauled home with paging of playlists

--- modules/playlist.js
+++ modules/playlist.js
@@ -10,11 +10,14 @@ var authorize = require(path.join(customModulePath, 'authorize.js'));
 // Playlist Logic
 const spotifyGetUserPlaylistsUri = 'https://api.spotify.com/v1/me/playlists';
 
+const userPlaylistRequestLimitDefault = 9;
+const userPlaylistPageNumberDefault = 1;
+
 exports.getAllUserPlaylists = async function(req, res)
 {
-    // TODO - Make this configurable so a user can control how many to fetch at once in the UI layer and utilize paging
-    var userPlaylistRequestLimit = 50;
-    var userPlaylistRequestOffset = 0;
+    var userPlaylistRequestLimit = req.query.playlistsPerPage || userPlaylistRequestLimitDefault;
+    var userPlaylistPageNumber = req.query.pageNumber || userPlaylistPageNumberDefault;
+    var userPlaylistRequestOffset = (userPlaylistPageNumber - 1) * userPlaylistRequestLimit;
 
     // Make the request to get all playlist data for this user
     var requestData = {
