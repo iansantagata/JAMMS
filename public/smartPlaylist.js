c6fa9ff9104dc6e5e1bf39c3f9fe3cad024a8f04
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-08-16 13:49:57 -0400

    Add functionality for removal of smart playlist rule

--- public/smartPlaylist.js
+++ public/smartPlaylist.js
@@ -1,18 +1,44 @@
 // Script Logic
+var lastActiveRuleIndex = 0;
+
 addOnClickEventListenerToElementById("playlistLimitEnabledInput", controlEnablementOfLimitElements);
 addOnClickEventListenerToElementById("playlistOrderEnabledInput", controlEnablementOfOrderElements);
+
+addOnClickEventListenerToElementById("removeRuleButton-" + lastActiveRuleIndex, removeRuleFormFields);
 
 // DOM Specific Functions
 function controlEnablementOfLimitElements()
 {
-  controlEnablementOfElementById("playlistLimitValueInput");
-  controlEnablementOfElementById("playlistLimitTypeInput");
+    controlEnablementOfElementById("playlistLimitValueInput");
+    controlEnablementOfElementById("playlistLimitTypeInput");
 }
 
 function controlEnablementOfOrderElements()
 {
-  controlEnablementOfElementById("playlistOrderDirectionInput");
-  controlEnablementOfElementById("playlistOrderFieldInput");
+    controlEnablementOfElementById("playlistOrderDirectionInput");
+    controlEnablementOfElementById("playlistOrderFieldInput");
+}
+
+function removeRuleFormFields()
+{
+    // There may be multiple buttons for removal, see which one this is by ID
+    var eventElementId = event.currentTarget.id;
+    var index = eventElementId.lastIndexOf("-");
+    if (index === -1)
+    {
+        return;
+    }
+
+    var targetRuleNumber = eventElementId.substr(index + 1);
+    if (targetRuleNumber === "")
+    {
+        return;
+    }
+
+    // With the rule number, delete the entire rule from the DOM
+    var targetRuleId = "rule-" + targetRuleNumber;
+    var targetRuleElement = document.getElementById(targetRuleId);
+    targetRuleElement.remove();
 }
 
 // Generalized Helper Functions
@@ -24,7 +50,7 @@ function addOnClickEventListenerToElementById(id, callback)
 
 function controlEnablementOfElementById(id)
 {
-  var element = document.getElementById(id);
-  var isDisabled = element.disabled;
-  element.disabled = !isDisabled;
+    var element = document.getElementById(id);
+    var isDisabled = element.disabled;
+    element.disabled = !isDisabled;
 }
