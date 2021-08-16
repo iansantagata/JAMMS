bff4496cff75df3fabf75b35ba79d4f4710ad077
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-08-16 17:03:56 -0400

    Implement creation of smart playlist similar to generic playlist

--- modules/smartPlaylist.js
+++ modules/smartPlaylist.js
@@ -15,5 +15,39 @@ exports.createSmartPlaylistPage = async function(req, res, next)
 
 exports.createSmartPlaylist = async function(req, res, next)
 {
-    // TODO - Create the endpoint to create the smart playlist
+    // Create a new playlist based on the user's request parameters
+    try
+    {
+        // Only thing we do not have supplied from the user is their user ID
+        // The app has to get their user ID first to attach this new playlist to their profile
+        req.body.userId = await spotifyClient.getCurrentUserId(req, res);
+        var spotifyResponse = await spotifyClient.createSinglePlaylist(req, res);
+
+        // Now that we have created the playlist, we want to add songs to it based on the smart playlist rules
+        // TODO - DO THIS
+
+        // Finally, we want to show the user info about their new playlist, so retrieve that data after songs were inserted
+        // TODO - DO THIS
+    }
+    catch (error)
+    {
+        next(error);
+        return;
+    }
+
+    var playlistData = {
+        playlistId: spotifyResponse.id,
+        playlistName: spotifyResponse.name,
+        playlistDescription: spotifyResponse.description,
+        isCollaborative: spotifyResponse.collaborative,
+        isPublic: spotifyResponse.public,
+        followersCount: spotifyResponse.followers.total,
+        trackCount: spotifyResponse.tracks.total,
+        images: spotifyResponse.images,
+        deleted: false
+    };
+
+    // Shove the playlist response data onto the playlist page for the user to interact with
+    res.location('/playlist');
+    res.render('viewPlaylist', playlistData);
 }
