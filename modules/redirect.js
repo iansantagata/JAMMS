c08d999207f8238e1db68fff02c906c8951061e6
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-09-10 21:57:22 -0400

    Adding debug logs for https bug

--- modules/redirect.js
+++ modules/redirect.js
@@ -11,6 +11,8 @@ exports.getBaseUri = function(req)
 
     // Build the full Uri to work both in production and while developing via localhost
     // TODO - Make this https when in production and http when in development
+    console.log('THIS IS THE PROTOCOL: ' + req.protocol);
+    console.log('THIS IS THE HOST NAME:' + req.hostname);
     return req.protocol + '://' + hostName;
 }
 
