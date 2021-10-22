ce6c6caa3255b0e8f63553321133882d2f030876
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-10-22 19:49:18 -0400

    Add special rule for beats per minute to flag a need for audio features

--- modules/smartPlaylistModules/specialRules.js
+++ modules/smartPlaylistModules/specialRules.js
@@ -15,6 +15,7 @@ const rules = require(path.join(smartPlaylistModulesPath, "rules.js"));
 
 // Default Constant Values
 const artistGenreRetrievalLimit = 50;
+const audioFeaturesRetrievalLimit = 100;
 
 // Special Rules Logic
 exports.getPlaylistSpecialRuleFlags = function(inputRules)
@@ -31,6 +32,11 @@ exports.getPlaylistSpecialRuleFlags = function(inputRules)
         if (inputRule.function === rules.ruleByGenre)
         {
             flags.add("genre");
+        }
+
+        if (inputRule.function === rules.ruleByBeatsPerMinute)
+        {
+            flags.add("audioFeatures");
         }
     }
 
@@ -120,6 +126,78 @@ exports.getArtistIdToGenreMap = async function(req, res, savedTracks, existingAr
         // If there is a problem with getting artist data, then a user's playlist request related to genre data cannot realistically succeed
         // Best to log the error and reject to avoid a situation where users are built an undesireable smart playlist
         logger.logError(`Failed to build artist to genres map: ${error.message}`);
+        return Promise.reject(error);
+    }
+};
+
+exports.getTrackIdToAudioFeaturesMap = async function(req, res, savedTracks, existingTrackIdToAudioFeaturesMap)
+{
+    try
+    {
+        // First, get a set of all unique track IDs that do not exist in our map already
+        const unmappedTrackIds = new Set();
+        for (const savedTrack of savedTracks)
+        {
+            // If we cannot find data about a track or ID, just skip the track
+            if (!savedTrack || !savedTrack.track || !savedTrack.track.id)
+            {
+                continue;
+            }
+
+            // Look to see that the ID is present and valid and does not already exist in our map
+            // If it does exist in the map, we can safely skip it (since we already know that song's audio features)
+            const trackId = savedTrack.track.id;
+            if (trackId && !existingTrackIdToAudioFeaturesMap.has(trackId))
+            {
+                unmappedTrackIds.add(trackId);
+            }
+        }
+
+        // If it turns out all of the tracks we have are already mapped, then we can just return the existing map
+        if (unmappedTrackIds.size <= 0)
+        {
+            return Promise.resolve(existingTrackIdToAudioFeaturesMap);
+        }
+
+        // With a set of unique unmapped track IDs, group them into chunks to get audio feature data for tracks in batches
+        const trackIds = Array.from(unmappedTrackIds);
+        const trackIdChunks = helperFunctions.getArrayChunks(trackIds, audioFeaturesRetrievalLimit);
+        const trackIdToAudioFeaturesMap = new Map();
+
+        for (const trackIdChunk of trackIdChunks)
+        {
+            // Call out to Spotify to get audio feature information for not already mapped tracks
+            req.query.trackIds = trackIdChunk;
+            const response = await spotifyClient.getAudioFeatures(req, res);
+            if (!response || !response.audioFeatures)
+            {
+                throw new Error("Failed to get valid audio features response data");
+            }
+
+            // Loop through all of the audio feature responses and store in the map (one audio feature object for each track)
+            for (const audioFeatures of response.audioFeatures)
+            {
+                if (!audioFeatures)
+                {
+                    continue;
+                }
+
+                // Add the audio features object into the map corresponding to the track ID
+                trackIdToAudioFeaturesMap.set(audioFeatures.id, audioFeatures);
+            }
+        }
+
+        // Finally, take the track IDs to audio features we found with this run
+        // Then add them to the existing map to return
+        const updatedTrackIdToAudioFeaturesMap = new Map([...existingTrackIdToAudioFeaturesMap, ...trackIdToAudioFeaturesMap]);
+        return Promise.resolve(updatedTrackIdToAudioFeaturesMap);
+    }
+    catch (error)
+    {
+        // If there is a problem with getting audio feature data,
+        // Then a user's playlist request related to audio feature data cannot realistically succeed
+        // Best to log the error and reject to avoid a situation where users are built an undesireable smart playlist
+        logger.logError(`Failed to build track to audio features map: ${error.message}`);
         return Promise.reject(error);
     }
 };
