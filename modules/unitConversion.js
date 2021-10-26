f5adfbf5f9c8c2c81622ac3e8ea57639125e4ff1
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-10-26 12:48:40 -0400

    Fix lint

--- modules/unitConversion.js
+++ modules/unitConversion.js
@@ -11,54 +11,54 @@ const hoursToMinutesConversion = 60;
 exports.getMillisecondsFromSeconds = function(seconds)
 {
     return seconds * secondsToMsecConversion;
-}
+};
 
 exports.getSecondsFromMinutes = function(minutes)
 {
     return minutes * minutesToSecondsConversion;
-}
+};
 
 exports.getMinutesFromHours = function(hours)
 {
     return hours * hoursToMinutesConversion;
-}
+};
 
 exports.getSecondsFromMilliseconds = function(msec)
 {
     return msec / secondsToMsecConversion;
-}
+};
 
 exports.getMinutesFromSeconds = function(seconds)
 {
     return seconds / minutesToSecondsConversion;
-}
+};
 
 exports.getHoursFromMinutes = function(minutes)
 {
     return minutes / hoursToMinutesConversion;
-}
+};
 
 // Multiple Step Conversions
 exports.getMillisecondsFromHours = function(hours)
 {
     const minutes = exports.getMinutesFromHours(hours);
     return exports.getMillisecondsFromMinutes(minutes);
-}
+};
 
 exports.getMillisecondsFromMinutes = function(minutes)
 {
     const seconds = exports.getSecondsFromMinutes(minutes);
     return exports.getMillisecondsFromSeconds(seconds);
-}
+};
 
 exports.getHoursFromMilliseconds = function(msec)
 {
     const minutes = exports.getMinutesFromMilliseconds(msec);
     return exports.getHoursFromMinutes(minutes);
-}
+};
 
 exports.getMinutesFromMilliseconds = function(msec)
 {
     const seconds = exports.getSecondsFromMilliseconds(msec);
     return exports.getMinutesFromSeconds(seconds);
-}
+};
