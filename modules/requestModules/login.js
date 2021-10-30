33c02d9398b7e74a45b59e76696773bd8ae8a700
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-10-29 22:19:57 -0400

    Move login module to request modules

--- modules/requestModules/login.js
+++ modules/requestModules/login.js
@@ -4,7 +4,7 @@
 const path = require("path"); // URI and local file paths
 
 // Utility Modules
-const utilityModulesPath = path.join(__dirname, "utilityModules");
+const utilityModulesPath = path.join(__dirname, "..", "utilityModules");
 const logger = require(path.join(utilityModulesPath, "logger.js"));
 const environment = require(path.join(utilityModulesPath, "environment.js"));
 const loginUtils = require(path.join(utilityModulesPath, "loginUtils.js"));
