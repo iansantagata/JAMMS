85f67dc763fbb6a766ad92569339e9c5d19af9c9
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-10-30 14:28:23 -0400

    Fix login bug on contact page displaying incorrect headers

--- modules/requestModules/contact.js
+++ modules/requestModules/contact.js
@@ -6,15 +6,23 @@ const path = require("path"); // URI and local file paths
 // Utility Modules
 const utilityModulesPath = path.join(__dirname, "..", "utilityModules");
 const logger = require(path.join(utilityModulesPath, "logger.js"));
+const loginUtils = require(path.join(utilityModulesPath, "loginUtils.js"));
 
 // Contact Logic
-exports.getContactPage = function(req, res, next)
+exports.getContactPage = async function(req, res, next)
 {
     try
     {
+        // See if the user is already logged in to determine what to show them on the contact page
+        const isUserLoggedIn = await loginUtils.isUserLoggedIn(req, res);
+
+        const contactPageData = {
+            isAwaitingLogin: !isUserLoggedIn
+        };
+
         // Render the contact page that the user can interact with
         res.location("/contact");
-        res.render("contact");
+        res.render("contact", contactPageData);
     }
     catch (error)
     {
