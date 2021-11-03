b3cb1cf4f7fcb145dfbffae217e043ef6b24722f
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2021-11-03 00:04:03 -0400

    Adjust grunt file to use grunt-contrib-copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,22 @@ module.exports = function(grunt)
 {
     // Project Configuration
     grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json")
+        pkg: grunt.file.readJSON("package.json"),
+        copy: {
+            files: {
+                expand: true,
+                flatten: true,
+                dest: "",
+                cwd: "",
+                src: "",
+                filter: "isFile"
+            }
+        }
     });
 
     // Load Plugins
-    grunt.loadNpmTasks("");
+    grunt.loadNpmTasks("grunt-contrib-copy");
 
     // Default Tasks
-    grunt.registerTask("default", [""]);
+    grunt.registerTask("default", ["copy"]);
 };
